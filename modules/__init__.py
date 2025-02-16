9a623613d6bd9dbd4dc54a981d978e9cdba27af1
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2025-02-16 20:53:46 +0800

    Remove visualizer module from module exports

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,7 +1,6 @@
-from .algorithms import heuristic, astar_step, dijkstra_step
-from .grid import create_grid  # Remove START, GOAL from import
+from .grid import create_grid
+from .algorithms import astar_step, dijkstra_step
 from .agent import PathAgent
-from .visualizer import Maze2DVisualizer
+from .ghost import Ghost
 
-__all__ = ['Maze2DVisualizer', 'astar_step', 'dijkstra_step', 
-           'heuristic', 'PathAgent', 'create_grid']
+__all__ = ['astar_step', 'dijkstra_step', 'PathAgent', 'create_grid']
